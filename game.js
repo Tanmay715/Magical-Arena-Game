fa624d0ed7cefac6e1323fba8708787ad88073f1
Tanmay Tripathi <tanmaytripathi348@gmail.com>
2024-01-27 12:55:38 +0530

    Writing calculation logic for Attack and defence numbers

--- game.js
+++ game.js
@@ -4,6 +4,34 @@ class MagicalArena {
     constructor(PlayerA, PlayerB) {
         this.PlayerA = PlayerA;
         this.PlayerB = PlayerB;
-        this.mainPlayer = this.PlayerA.health < this.PlayerB.health ? 'A' : 'B';
+        this.rollingPlayer = this.PlayerA.health < this.PlayerB.health ? 'A' : 'B';
     }
+
+    nextTurn() {
+        this.rollingPlayer == 'A' ? 'B' : 'A';
+    }
+
+    attackAndDefend(PlayerA, PlayerB) {
+        const attacker = this.rollingPlayer == 'A' ? PlayerA : PlayerB;
+        const defender = attacker == PlayerA ? PlayerB : PlayerA;
+
+        const attackNumber = attacker.rollDice();
+        const defenceNumber = defender.rollDice();
+        
+        const attackValue = attackNumber * (attacker.attack);
+        const defenceValue = defenceNumber * (defender.strength);
+        
+        const damageValue = Math.max(0, attackValue - defenceValue);
+        defender.health -= damageValue;
+    }
+
+    startGame(PlayerA, PlayerB) {
+        while (PlayerA.health > 0 && PlayerB.health > 0) {
+            this.attackAndDefend(PlayerA, PlayerB);
+        }
+
+    }
+
 }
+
+module.exports = MagicalArena;
