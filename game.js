acf1a0facff5a91a79316a5f954584f1aa95b6ea
Tanmay Tripathi <tanmaytripathi348@gmail.com>
2024-01-27 12:37:26 +0530

    Writing main game class

--- game.js
+++ game.js
@@ -1,3 +1,9 @@
+const Player = require('./src/player');
+
 class MagicalArena {
-    
+    constructor(PlayerA, PlayerB) {
+        this.PlayerA = PlayerA;
+        this.PlayerB = PlayerB;
+        this.mainPlayer = this.PlayerA.health < this.PlayerB.health ? 'A' : 'B';
+    }
 }
